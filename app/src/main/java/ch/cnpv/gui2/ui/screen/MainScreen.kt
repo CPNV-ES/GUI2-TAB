9f6fc3a04a01fb3d271f31cc4b3a68f6706979f3
Sytabram <bryan.zweiacker@sytabram.ch>
2025-11-24 11:32:53 +0100

    Feat: Add carousel stories to MainScreen

--- app/src/main/java/ch/cnpv/gui2/ui/screen/MainScreen.kt
+++ app/src/main/java/ch/cnpv/gui2/ui/screen/MainScreen.kt
@@ -1,17 +1,18 @@
 package ch.cnpv.gui2.ui.screen
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.AddCircle
-import androidx.compose.material.icons.sharp.AddCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -23,14 +24,20 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.carousel.HorizontalUncontainedCarousel
+import androidx.compose.material3.carousel.rememberCarouselState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ch.cnpv.gui2.R
 import ch.cnpv.gui2.ui.theme.AppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -87,6 +94,7 @@ fun MainScreen(onNavigateOtherClick: () -> Unit) {
         }
     ) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
+            CarouselStories()
             Text("MainScreen - Onglet: ${items[selectedItem]}")
             Button(
                 onClick = { onNavigateOtherClick() },
@@ -94,6 +102,44 @@ fun MainScreen(onNavigateOtherClick: () -> Unit) {
                 Text("Navigate to other screen")
             }
         }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CarouselStories() {
+    data class CarouselItem(
+        val id: Int,
+        @DrawableRes val imageResId: Int,
+        val contentDescription: String
+    )
+
+    val carouselItems = remember {
+        listOf(
+            CarouselItem(0, R.drawable.profile1, "cupcake"),
+            CarouselItem(1, R.drawable.profile1, "donut"),
+            CarouselItem(2, R.drawable.profile1, "eclair"),
+            CarouselItem(3, R.drawable.profile1, "froyo"),
+            CarouselItem(4, R.drawable.profile1, "gingerbread"),
+        )
+    }
+
+    HorizontalUncontainedCarousel(
+        state = rememberCarouselState { carouselItems.count() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(top = 16.dp, bottom = 16.dp),
+        itemWidth = 100.dp,
+        itemSpacing = 8.dp,
+    ) { i ->
+        val item = carouselItems[i]
+        Image(
+            modifier = Modifier
+                .height(100.dp),
+            painter = painterResource(id = item.imageResId),
+            contentDescription = item.contentDescription,
+            contentScale = ContentScale.Fit
+        )
     }
 }
 
