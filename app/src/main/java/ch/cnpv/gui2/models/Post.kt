2699ec85ffc8d41b6c9310e52fda468b93878ce7
TheoPilet <theo.pilet@bluewin.ch>
2025-12-15 01:23:22 +0100

    feat: add Comments

--- app/src/main/java/ch/cnpv/gui2/models/Post.kt
+++ app/src/main/java/ch/cnpv/gui2/models/Post.kt
@@ -1,8 +1,10 @@
 package ch.cnpv.gui2.models
 
 data class Post(
+    val id: Int,
     val profil: Profil,
     val description: String,
     val topic : String,
-    val image: Int
+    val image: Int,
+    val comments: List<Comment>
     ){}
