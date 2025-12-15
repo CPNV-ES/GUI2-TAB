4f83577dde4fca4e89883347a451ab56b950f971
TheoPilet <theo.pilet@bluewin.ch>
2025-12-15 01:30:28 +0100

    feat: DetailScreen

--- app/src/main/java/ch/cnpv/gui2/ui/screen/MainScreen.kt
+++ app/src/main/java/ch/cnpv/gui2/ui/screen/MainScreen.kt
@@ -3,23 +3,42 @@ package ch.cnpv.gui2.ui.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ch.cnpv.gui2.data.Data
+import ch.cnpv.gui2.models.Post
 import ch.cnpv.gui2.ui.theme.AppTheme
 
 @Composable
-fun MainScreen(onNavigateOtherClick: () -> Unit) {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text("MainScreen")
+fun MainScreen(
+    posts: List<Post>,
+    onPostClick: (Post) -> Unit,
+    onNavigateOtherClick: () -> Unit,
+) {
+    Scaffold { padding ->
+        Column(modifier = Modifier.padding(padding)) {
             Button(
                 onClick = { onNavigateOtherClick() },
             ) {
                 Text("Navigate to other screen")
+            }
+            LazyColumn(modifier = Modifier.padding(padding)) {
+                items(posts) { post ->
+                    Column() {
+                        Text(text = post.profil.username)
+                        Text(text = post.description)
+
+                        Button(onClick = { onPostClick(post) }) {
+                            Text("Voir détails")
+                        }
+                    }
+                }
             }
         }
     }
@@ -29,6 +48,10 @@ fun MainScreen(onNavigateOtherClick: () -> Unit) {
 @Composable
 fun MainScreenPreview() {
     AppTheme {
-        MainScreen(onNavigateOtherClick = {})
+        MainScreen(
+            posts = Data.posts,
+            onPostClick = {},
+            onNavigateOtherClick = {},
+        )
     }
 }
